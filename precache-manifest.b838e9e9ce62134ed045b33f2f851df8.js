6a9ce0e02c85399fea63dca873c0451f29c38a1e
Anamika Katyal <Anamika.Katyal@tatacommunications.com>
2020-09-27 22:14:03 +0530

    Updates

---
+++ precache-manifest.b838e9e9ce62134ed045b33f2f851df8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72de5e0d83ae0b6e869ad0caefc20324",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/index.html"
+  },
+  {
+    "revision": "cb62e634dea8166e41d6",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/css/main.69df923b.chunk.css"
+  },
+  {
+    "revision": "6575a1a99805bf2a0950",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/js/2.76775c1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/js/2.76775c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb62e634dea8166e41d6",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/js/main.71de66c2.chunk.js"
+  },
+  {
+    "revision": "71969d71b1520a41ef5c",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/js/runtime-main.e8446528.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FUNWITJS-1/tooltip.github.io.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
